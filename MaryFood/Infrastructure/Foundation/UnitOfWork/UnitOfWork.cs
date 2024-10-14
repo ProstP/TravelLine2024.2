4acebcb3556582e611021601619733f034e14e16
LAPTOP-U0R8E398\prost <prosto_channal@mail.ru>
2024-10-14 08:00:58 +0300

    Добавил UnitOfWorks

---
+++ MaryFood/Infrastructure/Foundation/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using App.UnitOfWork;
+
+namespace Infrastructure.Foundation.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        MaryFoodDbContext _dbContext;
+
+        public UnitOfWork( MaryFoodDbContext dbContext )
+        {
+            _dbContext = dbContext;
+        }
+
+        public async void SaveChanges()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
