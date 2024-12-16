70d460a1245c1294caf624a64878012fa85073df
LAPTOP-U0R8E398\prost <prosto_channal@mail.ru>
2024-12-16 18:37:47 +0300

    Добавил поля для поиска рецепта по названию

--- src/Frontend/src/components/RecipeListPage/RecipeListPage.tsx
+++ src/Frontend/src/components/RecipeListPage/RecipeListPage.tsx
@@ -1,9 +1,16 @@
 import styles from "./RecipeListPage.module.scss";
 import RecipeList from "../RecipeList/RecipeList";
+import SearchRecipeInput from "../SearchRecipeInput/SearchRecipeInput";
 
 const RecipeListPage = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.searchfield}>
+        <p className={styles.searchtitle}>Поиск рецепта</p>
+        <SearchRecipeInput
+          onClick={(text) => console.log(text)}
+        ></SearchRecipeInput>
+      </div>
       <RecipeList></RecipeList>
     </div>
   );
