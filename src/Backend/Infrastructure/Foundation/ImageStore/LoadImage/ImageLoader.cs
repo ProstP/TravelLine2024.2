2df04fa2ed3f2563cb2fd0798eaabbe118a55957
LAPTOP-U0R8E398\prost <prosto_channal@mail.ru>
2024-12-25 15:17:02 +0300

    Сделал реализацию для загрузки изображения и перевода его в base64

--- src/Backend/Infrastructure/Foundation/ImageStore/LoadImage/ImageLoader.cs
+++ src/Backend/Infrastructure/Foundation/ImageStore/LoadImage/ImageLoader.cs
@@ -6,6 +6,24 @@ public class ImageLoader : IImageLoader
 {
     public string Load( string path )
     {
-        throw new NotImplementedException();
+        string folderPath = Path.Combine( Directory.GetCurrentDirectory(), "Images" );
+
+        string filePath = Path.Combine( folderPath, path );
+
+        if ( !Directory.Exists( folderPath ) )
+        {
+            return "";
+        }
+
+        if ( !File.Exists( filePath ) )
+        {
+            return "";
+        }
+
+        byte[] imageBytes = File.ReadAllBytes( filePath );
+
+        string imageBase64 = Convert.ToBase64String( imageBytes );
+
+        return imageBase64;
     }
 }
