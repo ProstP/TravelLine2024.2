8ffbaaeb286398c31bdd26d1ab78992c3d039d56
LAPTOP-U0R8E398\prost <prosto_channal@mail.ru>
2025-01-14 17:18:55 +0300

    Сделал репозиторий избранного

---
+++ src/Backend/Infrastructure/Foundation/Repository/FavouriteRepository.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entity;
+using Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Foundation.Repository;
+
+public class FavouriteRepository : Repository<Favourite>, IFavouriteRepository
+{
+    public FavouriteRepository( MaryFoodDbContext dbContext )
+        : base( dbContext )
+    { }
+
+    public async Task<int> GetByRecipeId( int recipeId )
+    {
+        return await DbSet.Where( f => f.RecipeId == recipeId )
+                          .CountAsync();
+    }
+
+    public async Task<int> GetByUserId( int userId )
+    {
+        return await DbSet.Where( f => f.UserId == userId )
+                          .CountAsync();
+    }
+
+    public async Task<bool> IsExist( int userId, int recipeId )
+    {
+        return await DbSet.AnyAsync( f => f.UserId == userId && f.RecipeId == recipeId );
+    }
+}
